9d05ee2412e6b2e95054dea0985b33cdf132f5a3
penchenatremy <finance@vedrax.com>
2020-03-14 19:39:22 +0200

    ADD GCP UTIL

---
+++ vedrax-util/src/main/java/com/vedrax/util/GCPUtil.java
@@ -0,0 +1,21 @@
+package com.vedrax.util;
+
+import org.apache.commons.lang3.Validate;
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskOptions;
+
+import java.util.Map;
+
+public class GCPUtil {
+
+  public static void runTaskWithParams(String endpoint, Map<String, String> params) {
+    Validate.notNull(endpoint, "endpoint must be provided");
+    Validate.notEmpty(params, "params must be provided");
+
+    Queue queue = QueueFactory.getDefaultQueue();
+    TaskOptions options = TaskOptions.Builder.withUrl(endpoint);
+    params.forEach(options::param);
+    queue.add(options);
+  }
+}
