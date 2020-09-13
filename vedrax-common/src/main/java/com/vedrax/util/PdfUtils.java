2730d03bd26ff8455705342c010a166d3e429701
Remy Penchenat <finance@vedrax.com>
2020-09-13 13:00:14 +0300

    improve code

--- vedrax-common/src/main/java/com/vedrax/util/PdfUtils.java
+++ vedrax-common/src/main/java/com/vedrax/util/PdfUtils.java
@@ -4,6 +4,7 @@ import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.FontSelector;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
+import org.apache.commons.lang3.Validate;
 import org.springframework.util.Assert;
 
 import java.math.BigDecimal;
@@ -30,53 +31,73 @@ public class PdfUtils {
     public static Font font14o = FontFactory.getFont(FontFactory.COURIER_OBLIQUE, 14, BaseColor.BLACK);
     public static Font font14b = FontFactory.getFont(FontFactory.COURIER_BOLD, 14, BaseColor.BLACK);
 
-    public static PdfPCell getAddress(String companyName, String address, String zip, String city) {
+    /**
+     * Method for displaying address
+     *
+     * @param companyName the company name
+     * @param address     the company address
+     * @param zip         the company postal code
+     * @param city        the city of the company
+     * @return the formatted company address
+     */
+    public static PdfPCell getAddress(String companyName,
+                                      String address,
+                                      String zip,
+                                      String city) {
         PdfPCell cell = new PdfPCell();
         cell.setBorder(Rectangle.NO_BORDER);
         cell.setVerticalAlignment(Rectangle.ALIGN_MIDDLE);
-        Paragraph p = new Paragraph(companyName, font12b);
-        cell.addElement(p);
-        p = new Paragraph(address, font12);
-        cell.addElement(p);
-        p = new Paragraph(String.format("%s %s", zip, city), font12);
-        cell.addElement(p);
+        cell.addElement(createParagraph(companyName, font12b));
+        cell.addElement(createParagraph(address, font12));
+        cell.addElement(createParagraph(String.format("%s %s", zip, city), font12));
         return cell;
     }
 
-    public static PdfPCell getAddressRed(String companyName, String address, String zip, String city) {
-        PdfPCell cell = new PdfPCell();
-        cell.setBorder(Rectangle.NO_BORDER);
-        cell.setVerticalAlignment(Rectangle.ALIGN_MIDDLE);
-        Paragraph p = new Paragraph(companyName, font12bRed);
-        cell.addElement(p);
-        p = new Paragraph(address, font12Red);
-        cell.addElement(p);
-        p = new Paragraph(String.format("%s %s", zip, city), font12Red);
-        cell.addElement(p);
-        return cell;
+    /**
+     * Method for creating a paragraph
+     *
+     * @param value the value to display
+     * @param font  the font to be used
+     * @return the paragraph
+     */
+    public static Paragraph createParagraph(String value, Font font) {
+        value = value == null ? "" : value;
+
+        if (font == null) {
+            return new Paragraph(value);
+        }
+
+        return new Paragraph(value, font);
     }
 
-    public static PdfPTable getNameValuePair(String title, List<CellItem> values, int verticalAlignment) throws DocumentException {
-        PdfPTable nvps = getNameValuePair(values, verticalAlignment);
-        return getOneColumnLayout(title, nvps);
+    /**
+     * Method for displaying NVP
+     *
+     * @param title               the bax title
+     * @param values              the values to be displayed
+     * @param horizontalAlignment the horizontal alignment
+     * @return table
+     * @throws DocumentException exception
+     */
+    public static PdfPTable getNameValuePair(String title,
+                                             List<CellItem> values,
+                                             int horizontalAlignment)
+            throws DocumentException {
+        return getOneColumnLayout(title, getNameValuePair(values, horizontalAlignment));
     }
 
-    public static PdfPTable getNote(String note) throws DocumentException {
-        PdfPTable table = getTable(1, new int[]{1}, Rectangle.NO_BORDER,
-                Element.ALIGN_LEFT, Element.ALIGN_LEFT);
-
-        table.addCell(getCell(font8, note, Rectangle.NO_BORDER, Element.ALIGN_LEFT, Element.ALIGN_LEFT, BaseColor.WHITE));
-        return table;
-    }
-
-    public static PdfPTable getNote(String title, String note) throws DocumentException {
-        PdfPTable table = getOneColumnLayout(title, getCell(font8, note, Rectangle.BOX, Element.ALIGN_LEFT, Element.ALIGN_LEFT, BaseColor.WHITE));
-        table.setSpacingBefore(10);
-        return table;
-    }
-
-    public static PdfPTable getNameValuePair(List<CellItem> values, int horizontalAlignment) throws DocumentException {
-        Assert.notEmpty(values, "values must be provided");
+    /**
+     * Method for displaying values as NVP
+     *
+     * @param values              the values to be displayed
+     * @param horizontalAlignment the horizontal alignment
+     * @return table
+     * @throws DocumentException exception
+     */
+    public static PdfPTable getNameValuePair(List<CellItem> values,
+                                             int horizontalAlignment)
+            throws DocumentException {
+        Validate.notEmpty(values, "values must be provided");
 
         PdfPTable table = getTable(2, new int[]{1, 1}, Rectangle.NO_BORDER,
                 horizontalAlignment, Element.ALIGN_LEFT);
@@ -88,13 +109,22 @@ public class PdfUtils {
         return table;
     }
 
-    public static PdfPTable getOneColumnLayout(String title, PdfPTable items) throws DocumentException {
-        Assert.notNull(title, "title must be provided");
-        Assert.notNull(items, "items must be provided");
+    /**
+     * Method for creating a one-column layout
+     *
+     * @param title the title
+     * @param items the items to be displayed as table
+     * @return table
+     * @throws DocumentException exception
+     */
+    public static PdfPTable getOneColumnLayout(String title,
+                                               PdfPTable items)
+            throws DocumentException {
+        Validate.notNull(title, "title must be provided");
+        Validate.notNull(items, "items must be provided");
 
         PdfPTable table = getTable(1, new int[]{1}, Rectangle.BOX,
                 Element.ALIGN_CENTER, Element.ALIGN_LEFT);
-        //table.setSpacingBefore(10f);
 
         table.addCell(getCell(font10b, title, Rectangle.BOX, Element.ALIGN_CENTER, Element.ALIGN_CENTER, BaseColor.LIGHT_GRAY));
         table.addCell(items);
@@ -102,9 +132,19 @@ public class PdfUtils {
         return table;
     }
 
-    public static PdfPTable getOneColumnLayout(String title, PdfPCell cell) throws DocumentException {
-        Assert.notNull(title, "title must be provided");
-        Assert.notNull(cell, "cell must be provided");
+    /**
+     * Method for creating a one-column layout
+     *
+     * @param title the title
+     * @param cell  the items to be displayed as cell
+     * @return table
+     * @throws DocumentException exception
+     */
+    public static PdfPTable getOneColumnLayout(String title,
+                                               PdfPCell cell)
+            throws DocumentException {
+        Validate.notNull(title, "title must be provided");
+        Validate.notNull(cell, "cell must be provided");
 
         PdfPTable table = getTable(1, new int[]{1}, Rectangle.BOX,
                 Element.ALIGN_CENTER, Element.ALIGN_LEFT);
@@ -116,14 +156,56 @@ public class PdfUtils {
         return table;
     }
 
+    /**
+     * Method for inserting note
+     *
+     * @param note the note
+     * @return table
+     * @throws DocumentException exception
+     */
+    public static PdfPTable getNote(String note)
+            throws DocumentException {
+        PdfPTable table = getTable(1, new int[]{1}, Rectangle.NO_BORDER,
+                Element.ALIGN_LEFT, Element.ALIGN_LEFT);
 
+        table.addCell(getCell(font8, note, Rectangle.NO_BORDER, Element.ALIGN_LEFT, Element.ALIGN_LEFT, BaseColor.WHITE));
+        return table;
+    }
+
+    /**
+     * Method for inserting note
+     *
+     * @param title the title
+     * @param note  the note
+     * @return table
+     * @throws DocumentException exception
+     */
+    public static PdfPTable getNote(String title,
+                                    String note)
+            throws DocumentException {
+        PdfPTable table = getOneColumnLayout(title, getCell(font8, note, Rectangle.BOX, Element.ALIGN_LEFT, Element.ALIGN_LEFT, BaseColor.WHITE));
+        table.setSpacingBefore(10);
+        return table;
+    }
+
+    /**
+     * Method for creating a two-column layout
+     *
+     * @param left                the left side as table
+     * @param right               the right side as table
+     * @param border              the border type
+     * @param horizontalAlignment the horizontal alignment
+     * @param verticalAlignment   the vertical alignment
+     * @return table
+     * @throws DocumentException exception
+     */
     public static PdfPTable getTwoColumnLayout(PdfPTable left,
                                                PdfPTable right,
                                                int border,
                                                int horizontalAlignment,
                                                int verticalAlignment) throws DocumentException {
-        Assert.notNull(left, "left element must be provided");
-        Assert.notNull(right, "Right element must be provided");
+        Validate.notNull(left, "left element must be provided");
+        Validate.notNull(right, "Right element must be provided");
 
         PdfPTable table = getTable(3, new int[]{10, 1, 10}, border,
                 horizontalAlignment, verticalAlignment);
@@ -144,13 +226,24 @@ public class PdfUtils {
         return table;
     }
 
+    /**
+     * Method for creating a two-column layout
+     *
+     * @param left                the left side as table
+     * @param right               the right side as cell
+     * @param border              the border type
+     * @param horizontalAlignment the horizontal alignment
+     * @param verticalAlignment   the vertical alignment
+     * @return table
+     * @throws DocumentException exception
+     */
     public static PdfPTable getTwoColumnLayout(PdfPTable left,
                                                PdfPCell right,
                                                int border,
                                                int horizontalAlignment,
                                                int verticalAlignment) throws DocumentException {
-        Assert.notNull(left, "left element must be provided");
-        Assert.notNull(right, "Right element must be provided");
+        Validate.notNull(left, "left element must be provided");
+        Validate.notNull(right, "Right element must be provided");
 
         PdfPTable table = getTable(3, new int[]{10, 1, 10}, border,
                 horizontalAlignment, verticalAlignment);
@@ -168,11 +261,25 @@ public class PdfUtils {
         return table;
     }
 
+    /**
+     * Method for creating a table
+     *
+     * @param numColumns          the number of columns
+     * @param widths              the widths of each column as an array of integer
+     * @param border              the border type
+     * @param horizontalAlignment the horizontal alignment
+     * @param verticalAlignment   the vertical alignment
+     * @return table
+     * @throws DocumentException exception
+     */
     public static PdfPTable getTable(int numColumns,
                                      int[] widths,
                                      int border,
                                      int horizontalAlignment,
                                      int verticalAlignment) throws DocumentException {
+        Validate.isTrue(numColumns > 0, "numColumns must be greater than 0");
+        Validate.isTrue(widths.length == numColumns, "The length of the widths values does not match");
+
         PdfPTable table = new PdfPTable(numColumns);
         table.setWidthPercentage(100);
         table.setWidths(widths);
@@ -183,13 +290,24 @@ public class PdfUtils {
         return table;
     }
 
+    /**
+     * Method for creating a cell
+     *
+     * @param font                the font
+     * @param text                the cell value
+     * @param border              the border type
+     * @param horizontalAlignment the horizontal alignment
+     * @param verticalAlignment   the vertical alignment
+     * @param backgroundColor     the background color
+     * @return cell
+     */
     public static PdfPCell getCell(Font font,
                                    String text,
                                    int border,
                                    int horizontalAlignment,
                                    int verticalAlignment,
                                    BaseColor backgroundColor) {
-        Assert.notNull(font, "font must be provided");
+        Validate.notNull(font, "font must be provided");
 
         text = text == null ? "" : text;
         backgroundColor = backgroundColor == null ? BaseColor.WHITE : backgroundColor;
@@ -212,29 +330,37 @@ public class PdfUtils {
         return cell;
     }
 
+    /**
+     * Method for creating cell (header)
+     *
+     * @param title the cell value
+     * @return cell
+     */
     public static PdfPCell getCellAsTitle(String title) {
         return getCell(font8b, title, Rectangle.BOX, Element.ALIGN_CENTER, Element.ALIGN_CENTER, BaseColor.LIGHT_GRAY);
     }
 
-    public static PdfPCell getCellAsDefault(String text, int border, int horizontalAlignment) {
-        return getCell(font8, text, border, horizontalAlignment, Element.ALIGN_CENTER, BaseColor.WHITE);
+    /**
+     * Method for creating cell (value - text)
+     *
+     * @param text                the cell value as text
+     * @param horizontalAlignment the horizontal alignment
+     * @return cell
+     */
+    public static PdfPCell getCellAsDefault(String text, int horizontalAlignment) {
+        return getCell(font8, text, Rectangle.BOX, horizontalAlignment, Element.ALIGN_CENTER, BaseColor.WHITE);
     }
 
-    public static Phrase initCellAsText(String text) {
-        text = text == null ? "" : text;
-
-        FontSelector fs = new FontSelector();
-        fs.addFont(font10);
-        return fs.process(text);
+    /**
+     * Method for creating cell (value - number)
+     *
+     * @param value               the cell value as number
+     * @param decimal             the decimal number
+     * @param horizontalAlignment the horizontal alignment
+     * @return cell
+     */
+    public static PdfPCell getCellAsDefault(BigDecimal value, int decimal, int horizontalAlignment) {
+        return getCell(font8, toNumberFormat(value, decimal), Rectangle.BOX, horizontalAlignment, Element.ALIGN_CENTER, BaseColor.WHITE);
     }
-
-    public static Phrase initCellAsNumber(BigDecimal value, int decimal) {
-        value = value == null ? BigDecimal.ZERO : value;
-
-        FontSelector fs = new FontSelector();
-        fs.addFont(font10);
-        return fs.process(toNumberFormat(value, decimal));
-    }
-
 
 }
