b99c810aba8dc9c0511cb29d8839b4ac5d3704d3
Remy Penchenat <finance@vedrax.com>
2021-01-21 14:10:14 +0200

    add regex

--- vedrax-math/src/main/java/com/vedrax/math/NVP.java
+++ vedrax-math/src/main/java/com/vedrax/math/NVP.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Pattern;
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class NVP {
   private String key;
+  @Pattern(regexp = "^[+-]?(?=.?\\d)\\d*(\\.\\d{0,9})?$")
   private String value;
 }
